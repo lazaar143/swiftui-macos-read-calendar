10808ec19f4b166782ebaa15cbe111eb846adb45
Tomislav Lazar <tomislav@morgancode.com>
2023-09-04 18:45:48 +0200

    Initial commit of new application

---
+++ swiftui-macos-read-calendar/swiftui_macos_read_calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_macos_read_calendarApp.swift
+//  swiftui-macos-read-calendar
+//
+//  Created by Tomislav Lazar on 04.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct swiftui_macos_read_calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
